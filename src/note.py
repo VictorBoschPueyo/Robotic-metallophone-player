336360a9d84655f4f7498e950e0d23bdb9ac27e2
VictorBoschPueyo <victor.bopu@gmail.com>
2023-03-09 13:32:12 +0100

    Start coding

---
+++ src/note.py
@@ -0,0 +1,8 @@
+
+
+
+class Note:
+    def __init__(self, tone, duration):
+        self.tone = tone
+        self.duration = duration
+        pass
---
+++ src/settings.py
@@ -0,0 +1,25 @@
+
+
+def init():
+    ## Define global variables in this file
+    global toneDictLeft
+
+    toneDictLeft = { 'G1' : 0 ,
+                     'G#1': 0, 
+                     'A1' : 0 ,
+                     'A#1': 0, 
+                     'B1' : 0 ,
+                     'C2' : 0 ,
+                     'C#2': 0, 
+                     'D2' : 0 ,
+                     'D#2': 0, 
+                     'E2' : 0 ,
+                     'E#2': 0, 
+                     'F2' : 0 ,
+                     'F#2': 0 ,
+                     'G2' : 0 ,
+                     'G#2': 0 ,
+                     'A2' : 0 ,
+                     'A#2': 0 ,
+                     'B2' : 0 ,
+                     }
