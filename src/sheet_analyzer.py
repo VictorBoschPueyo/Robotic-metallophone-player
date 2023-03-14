34f89b5ab64a9b1aa82d8ce55c4278cdd97b89eb
VictorBoschPueyo <victor.bopu@gmail.com>
2023-03-14 13:36:53 +0100

    read imgs

--- src/sheet_analyzer.py
+++ src/sheet_analyzer.py
@@ -9,38 +9,37 @@ from note import Note
 from random import randint
 
 # Read templates
-path = "/masks/"
+path = "masks"
 penta_files = [
-    path + "penta2.png",
-    path + "penta.png"]
+    path + "/penta2.png",
+    path + "/penta.png"]
 sost_files = [
-    path + "sostingut-espai.png",
-    path + "sostingut-linia.png"]
+    path + "/sostingut-espai.png",
+    path + "/sostingut-linia.png"]
 bem_files = [
-    path + "bemoll-espai.png",
-    path + "bemoll-linia.png"]
+    path + "/bemoll-espai.png",
+    path + "/bemoll-linia.png"]
 negra_files = [
-    path + "negra.png",
-    path + "negra-gran.png"]
+    path + "/negra.png",
+    path + "/negra-gran.png"]
 blanca_files = [
-    path + "blanca-espai.png",
-    path + "blanca-espai-gran.png",
-    path + "blanca-linia.png",
-    path + "blanca-linia-gran.png"]
+    path + "/blanca-espai.png",
+    path + "/blanca-espai-gran.png",
+    path + "/blanca-linia.png",
+    path + "/blanca-linia-gran.png"]
 rodona_files = [
-    path + "rodona-espai.png",
-    path + "rodona-espai-gran.png",
-    path + "rodona-linia.png",
-    path + "rodona-linia-gran.png"]
+    path + "/rodona-espai.png",
+    path + "/rodona-espai-gran.png",
+    path + "/rodona-linia.png",
+    path + "/rodona-linia-gran.png"]
 
-'''penta_imgs = [cv2.imread(f, 0) for f in penta_files]
+penta_imgs = [cv2.imread(f, 0) for f in penta_files]
 sost_imgs = [cv2.imread(f, 0) for f in sost_files]
 bem_imgs = [cv2.imread(f, 0) for f in bem_files]
 negra_imgs = [cv2.imread(f, 0) for f in negra_files]
 blanca_imgs = [cv2.imread(f, 0) for f in blanca_files]
-rodona_imgs = [cv2.imread(f, 0) for f in rodona_files]'''
+rodona_imgs = [cv2.imread(f, 0) for f in rodona_files]
 
-a = cv2.imread("himne_alegria.png", 0)
 
 penta_lower, penta_upper, penta_thresh = 50, 150, 0.77
 sost_lower, sost_upper, sost_thresh = 50, 150, 0.70
