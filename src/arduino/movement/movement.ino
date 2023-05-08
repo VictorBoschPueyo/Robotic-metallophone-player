930cefa9d10c304abeb62521b5156b31508ce941
Victor Bosch Pueyo <victor.bopu@gmail.com>
2023-05-08 19:02:00 +0200

    physical movement done

--- src/arduino/movement/movement.ino
+++ src/arduino/movement/movement.ino
@@ -20,8 +20,8 @@ Servo servo_left;
 Servo servo_right;
 
 // Positions array
-int left_positions[25] = {0, 0, 0, -1035, -955, -875, -795, -710, -640, -555, -480, -400, 315, -233, -160, -80, 0, 75, 155, 234, 315, 390, 480, 555, 635};
-int right_positions[25] = {-635, -555, -475, -395, -315, -235, -155, -75, 0, 85, 160, 235, 315, 407, 480, 560, 640, 715, 795, 875, 955, 1035, 0, 0, 0};
+int left_positions[25] = {-635, -555, -475, -395, -315, -235, -155, -75, 0, 85, 160, 235, 315, 407, 480, 560, 640, 715, 795, 875, 955, 1035, 0, 0, 0};
+int right_positions[25] = {0, 0, 0, -1035, -955, -875, -795, -710, -640, -555, -480, -400, 315, -233, -160, -80, 0, 75, 155, 234, 315, 390, 480, 555, 635};
 
 // Functions
 void move_left_hand(int pos)
@@ -42,11 +42,22 @@ void move_right_hand(int pos)
   }
 }
 
+void move_motors(int left_pos, int right_pos){
+  left_hand.moveTo(left_positions[left_pos]);
+  right_hand.moveTo(right_positions[right_pos]);
+
+  while (left_hand.distanceToGo() != 0 || right_hand.distanceToGo() != 0)
+  {
+    left_hand.run();
+    right_hand.run();
+  } 
+}
+
 void left_Drumstick_cling()
 {
-  servo_left.write(88);
+  servo_left.write(85);
   delay(150);
-  servo_left.write(106);
+  servo_left.write(104);
   delay(500);
 }
 
@@ -58,8 +69,29 @@ void right_Drumstick_cling()
   delay(500);
 }
 
+int setOption(String left_ins,  String right_ins){
+
+  
+
+  if (((left_ins == "WW") || (left_ins == "PP")) && ((right_ins == "WW") || (right_ins == "PP"))){
+    return 0;
+  }
+  else if (!((left_ins == "WW") || (left_ins == "PP")) && ((right_ins == "WW") || (right_ins == "PP"))){
+    return 1;
+  }
+  else if (((left_ins == "WW") || (left_ins == "PP")) && !((right_ins == "WW") || (right_ins == "PP"))){
+    return 2;
+  }
+  else{
+    return 3;
+  }
+}
+
+
 void setup()
 {
+  Serial.begin(9600);
+
   // Set the maximum speed and acceleration of the stepper motors
   // Also set the current position as 0
   left_hand.setMaxSpeed(20000);
@@ -73,22 +105,24 @@ void setup()
   // Assign the pin to each servo
   servo_left.attach(left_pin);
   servo_right.attach(right_pin);
+
 }
 
 void loop()
 {
-  servo_left.write(106);
+  servo_left.write(104);
   servo_right.write(110);
 
   if (Serial.available() > 0)
   {
     String input = Serial.readString(); // ex. L09RPP
+    Serial.println(input);
 
-    String left_instruction = input [1:2];
-    String right_instruction = input [4:5];
+    String left_instruction = input.substring(1, 3);  // ex. 09
+    String right_instruction = input.substring(4, 6); // ex. PP
 
-    Serial.println(left_instruction);
-    Serial.println(right_instruction);
+    int left_position;
+    int right_position;
 
     if (left_instruction == "PP")
     {
@@ -96,9 +130,7 @@ void loop()
     }
     else if (left_instruction != "WW")
     {
-      int left_position = left_instruction.toInt();
-      Serial.println(left_position);
-      move_left_hand(left_position);
+      left_position = left_instruction.toInt();
     }
 
     if (right_instruction == "PP")
@@ -107,9 +139,25 @@ void loop()
     }
     else if (right_instruction != "WW")
     {
-      int right_position = right_instruction.toInt();
-      Serial.println(right_position);
+      right_position = right_instruction.toInt();
+    }
+
+    // OPTIONS
+    // 0: do nothing
+    // 1: move LEFT
+    // 2: move RIGHT
+    // 3: move BOTH
+    int option = setOption(left_instruction, right_instruction);
+    Serial.println(option);
+
+    if (option == 1){
+      move_left_hand(left_position);
+    }
+    else if (option == 2){
       move_right_hand(right_position);
+    }
+    else if (option == 3){
+      move_motors(left_position, right_position);
     }
   }
 }
