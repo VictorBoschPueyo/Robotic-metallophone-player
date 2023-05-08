9fe602c6c250cb0b9091484007b2838c4843ad60
VictorBoschPueyo <victor.bopu@gmail.com>
2023-05-08 17:58:21 +0200

    Update movement.ino

--- src/arduino/movement/movement.ino
+++ src/arduino/movement/movement.ino
@@ -1,6 +1,6 @@
 #include <AccelStepper.h>
 #include <Servo.h>
- 
+
 // Define the stepper motor driver pins
 #define dirPin_left 2
 #define stepPin_left 3
@@ -14,12 +14,52 @@
 // Create an instance of the AccelStepper class
 AccelStepper left_hand(AccelStepper::DRIVER, stepPin_left, dirPin_left);
 AccelStepper right_hand(AccelStepper::DRIVER, stepPin_right, dirPin_right);
- 
-// Create an instance of the servos
-Servo servo_right;
-Servo servo_left;
 
-void setup() {
+// Create an instance of the servos
+Servo servo_left;
+Servo servo_right;
+
+// Positions array
+int left_positions[25] = {0, 0, 0, -1035, -955, -875, -795, -710, -640, -555, -480, -400, 315, -233, -160, -80, 0, 75, 155, 234, 315, 390, 480, 555, 635};
+int right_positions[25] = {-635, -555, -475, -395, -315, -235, -155, -75, 0, 85, 160, 235, 315, 407, 480, 560, 640, 715, 795, 875, 955, 1035, 0, 0, 0};
+
+// Functions
+void move_left_hand(int pos)
+{
+  left_hand.moveTo(left_positions[pos]);
+  while (left_hand.distanceToGo() != 0)
+  {
+    left_hand.run();
+  }
+}
+
+void move_right_hand(int pos)
+{
+  right_hand.moveTo(right_positions[pos]);
+  while (right_hand.distanceToGo() != 0)
+  {
+    right_hand.run();
+  }
+}
+
+void left_Drumstick_cling()
+{
+  servo_left.write(88);
+  delay(150);
+  servo_left.write(106);
+  delay(500);
+}
+
+void right_Drumstick_cling()
+{
+  servo_right.write(91);
+  delay(150);
+  servo_right.write(110);
+  delay(500);
+}
+
+void setup()
+{
   // Set the maximum speed and acceleration of the stepper motors
   // Also set the current position as 0
   left_hand.setMaxSpeed(20000);
@@ -33,45 +73,43 @@ void setup() {
   // Assign the pin to each servo
   servo_left.attach(left_pin);
   servo_right.attach(right_pin);
-
 }
 
-void loop() {
+void loop()
+{
   servo_left.write(106);
-  delay(500);
-  servo_left.write(88);
-  delay(150);
-  servo_left.write(106);
-  delay(1500);
-
   servo_right.write(110);
-  delay(500);
-  servo_right.write(91);
-  delay(150);
-  servo_right.write(110);
-  delay(1500);
 
-  
-  left_hand.move(200);
-  right_hand.move(200);
+  if (Serial.available() > 0)
+  {
+    String input = Serial.readString(); // ex. L09RPP
 
-  while (left_hand.distanceToGo() != 0) {
-    left_hand.run();
+    String left_instruction = input [1:2];
+    String right_instruction = input [4:5];
+
+    Serial.println(left_instruction);
+    Serial.println(right_instruction);
+
+    if (left_instruction == "PP")
+    {
+      left_Drumstick_cling();
+    }
+    else if (left_instruction != "WW")
+    {
+      int left_position = left_instruction.toInt();
+      Serial.println(left_position);
+      move_left_hand(left_position);
+    }
+
+    if (right_instruction == "PP")
+    {
+      right_Drumstick_cling();
+    }
+    else if (right_instruction != "WW")
+    {
+      int right_position = right_instruction.toInt();
+      Serial.println(right_position);
+      move_right_hand(right_position);
+    }
   }
-  while (right_hand.distanceToGo() != 0) {
-    right_hand.run();
-  }
-  delay(1000);
-
-  left_hand.move(-200);
-  right_hand.move(-200);
-
-  while (left_hand.distanceToGo() != 0) {
-    left_hand.run();
-  }
-  while (right_hand.distanceToGo() != 0) {
-    right_hand.run();
-  }
-  delay(1000);
-
 }
