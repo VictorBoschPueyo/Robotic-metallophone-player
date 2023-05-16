e234d3ac8c9b6e4dd5f7933b2837083c3a83d925
VictorBoschPueyo <victor.bopu@gmail.com>
2023-05-16 18:49:00 +0200

    partiture treatment

--- src/partiture_std.py
+++ src/partiture_std.py
@@ -1,52 +1,24 @@
 import cv2
 import numpy as np
 
+# Load reference points in a numpy array
+reference_points = np.loadtxt(
+    'points_reference.txt', delimiter=',', dtype=np.int32)
 
-def straighten_sheet(img_path):
-    img = cv2.imread(img_path)
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_gray = cv2.GaussianBlur(img_gray, (5, 5), 0)
-    img_gray = cv2.Canny(img_gray, 75, 200)
+# Load points from the image in a numpy array
+image_points = np.loadtxt('points_photo_reference.txt',
+                          delimiter=',', dtype=np.int32)
 
-    contours, _ = cv2.findContours(
-        img_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# Load the image
+img = cv2.imread('sheets/partiture.jpg')
+ref_image = cv2.imread('sheets/points_template.png')
 
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    sheet_contour = contours[0]
+# Find the homography matrix
+M, _ = cv2.findHomography(image_points, reference_points)
 
-    peri = cv2.arcLength(sheet_contour, True)
-    approx = cv2.approxPolyDP(sheet_contour, 0.02 * peri, True)
+# Warp the image
+partiture_std = cv2.warpPerspective(
+    img, M, (ref_image.shape[1], 2*ref_image.shape[0]//3))
 
-    src = np.float32([approx[0], approx[1], approx[2], approx[3]])
-    dst = np.float32([[0, 0], [0, 750], [500, 750], [500, 0]])
-
-    M = cv2.getPerspectiveTransform(src, dst)
-    img = cv2.warpPerspective(img, M, (500, 750))
-
-    cv2.imshow("Sheet", img)
-    cv2.waitKey(0)
-
-
-straighten_sheet('sheets/sheet10.jpg')
-
-"""img = cv2.imread('sheet10.jpg')
-
-
-print(img.shape)
-
-src = np.array([[183, 45],   [763, 44],   [1282, 65],
-                [131, 829],  [751, 833],  [1331, 817],
-                [125, 1659], [741, 1677], [1339, 1661]], dtype=np.float32)
-
-
-dst = np.array([[0, 0],               [img.shape[1]/2, 0],              [img.shape[1], 0],
-                [0, img.shape[0]/2],  [img.shape[1]/2,
-                                       img.shape[0]/2], [img.shape[1], img.shape[0]/2],
-                [0, img.shape[0]],    [img.shape[1]/2, img.shape[0]],   [img.shape[1], img.shape[0]]], dtype=np.float32)
-
-M, _ = cv2.findHomography(src, dst)
-
-rectangular_sheet = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))
-
-cv2.imwrite('sheet10_fixed.jpg', rectangular_sheet)
-cv2.waitKey(0)"""
+cv2.imwrite('sheets/partiture_fixed.jpg', partiture_std)
+cv2.waitKey(0)
