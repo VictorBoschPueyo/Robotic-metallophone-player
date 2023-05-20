28921249c3d7bdc1c4e776474d110fb78ec9da60
VictorBoschPueyo <victor.bopu@gmail.com>
2023-05-20 13:51:01 +0200

    new partiture std

--- src/partiture_std.py
+++ src/partiture_std.py
@@ -3,22 +3,22 @@ import numpy as np
 
 # Load reference points in a numpy array
 reference_points = np.loadtxt(
-    'points_reference.txt', delimiter=',', dtype=np.int32)
+    'sheets/points_himne_alegria_full.txt', delimiter=',', dtype=np.int32)
 
 # Load points from the image in a numpy array
-image_points = np.loadtxt('points_photo_reference.txt',
+image_points = np.loadtxt('sheets/points_foto_himne_alegria.txt',
                           delimiter=',', dtype=np.int32)
 
 # Load the image
-img = cv2.imread('sheets/partiture.jpg')
-ref_image = cv2.imread('sheets/points_template.png')
+img = cv2.imread('sheets/foto_himne_alegria.jpg')
+ref = cv2.imread('sheets/himne_alegria_full.png')
 
 # Find the homography matrix
 M, _ = cv2.findHomography(image_points, reference_points)
 
 # Warp the image
 partiture_std = cv2.warpPerspective(
-    img, M, (ref_image.shape[1], 2*ref_image.shape[0]//3))
+    img, M, (ref.shape[1], ref.shape[0]))
 
-cv2.imwrite('sheets/partiture_fixed.jpg', partiture_std)
+cv2.imwrite('sheets/himne_alegria_fixed.jpg', partiture_std)
 cv2.waitKey(0)
--- src/select_points.py
+++ src/select_points.py
@@ -1,15 +1,11 @@
 import cv2
 
 # Load the image
-img = cv2.imread('sheets/points_template.png')
+foto = "himne_alegria_full.png"
+img = cv2.imread('sheets/' + foto)
 
-scale_percent = 50  # percent of original size
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dim = (width, height)
-
-# resize image
-img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+# Resize the image
+img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 
 # Create a list to store the points
 points = []
@@ -17,11 +13,11 @@ points = []
 # Define the event callback function
 
 
-def click_event(event, x, y):
+def click_event(event, x, y, flags, param):
     # Check if left mouse button is clicked
     if event == cv2.EVENT_LBUTTONDOWN:
         # Append the clicked point to the list
-        points.append((int(x*1.5), int(y*1.5)))
+        points.append((int(x*2), int(y*2)))
         # Display the point on the image
         cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
         cv2.imshow('image', img)
@@ -37,7 +33,7 @@ cv2.setMouseCallback('image', click_event)
 cv2.waitKey(0)
 
 # Save the points to a txt file
-with open('points_reference_re.txt', 'w') as file:
+with open('sheets/points_' + foto + '.txt', 'w') as file:
     for point in points:
         file.write(f'{point[0]},{point[1]}\n')
 
